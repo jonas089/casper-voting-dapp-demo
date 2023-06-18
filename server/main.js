2ccdae2cf228805140fa21fa041a738a85c4a2b0
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-06-18 09:16:55 +0200

    add server to voting project

---
+++ server/main.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import pkg from 'casper-js-sdk';
+import path from 'path';
+const {Contracts, CasperClient, DeployUtil} = pkg;
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+async function Server(){
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static(__dirname + 'public/static'));
+    var httpServer = http.createServer(app);
+    httpServer.listen(3002, () => {console.log("Running HTTP on: ", 3002);});
+
+    app.post('/send', async(req, res) => {
+        try{
+            const nodeAddress = "http://";
+            const signedJson = req.body.signedJson;
+            let signedDeploy = DeployUtil.deployFromJson(signedJson).unwrap();
+            signedDeploy.send(nodeAddress).then((response) => {
+                res.send(response);
+                return;
+            })
+            .catch((error) => {
+                res.send(error);
+                return;
+            })
+        }
+        catch(error){
+            console.log("Server Error: ", error);
+            return;
+        }
+    });
+}
+
+Server();
--- src/routes/Example.jsx
+++ src/routes/Example.jsx
@@ -37,9 +37,8 @@ export default function Example() {
             </div>
 
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Lock: {isLocked !== undefined ? isLocked.toString() : "undefined"}
+              {isLocked && <h1>Your Casper Wallet is locked! <br/> <button onClick={fnConnect} className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Unlock</button></h1>}
               <br />
-              Connected: {isConnected !== undefined ? isConnected.toString() : "undefined"}
             </p>
 
             <div className="mt-10 flex items-center justify-center gap-x-6">
